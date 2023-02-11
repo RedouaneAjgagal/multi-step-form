fcc43c4799709785923a7e8666b8c361f7830dcf
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-02-11 15:30:37 +0100

    total UI for desktop

--- src/components/total/Total.tsx
+++ src/components/total/Total.tsx
@@ -7,7 +7,7 @@ const Total: React.FC<Props> = (props) => {
     const total = `+$${props.total}/${props.yearly ? 'yr' : 'mo'}`;
     const totalMsg = `Total (${props.yearly ? 'per year' : 'per month'})`
     return (
-        <div className='flex justify-between items-center w-full max-w-[22rem] m-auto'>
+        <div className='flex justify-between items-center w-full max-w-[95%] m-auto md:max-w-[90%]'>
             <p className='text-gray-500/80 text-lg'>{totalMsg}</p>
             <span className='text-indigo-700 font-bold text-xl'>{total}</span>
         </div>
